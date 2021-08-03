61142383ce2784b49572f305d964301087b30e2e
Dishant Wadhwani <wadhwaniyash1234@gmail.com>
2021-08-03 22:35:17 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDv2DwcPkOwdNUO-29KQU3DVYvRX2An7lU",
+    authDomain: "hello-whiz-a3b9f.firebaseapp.com",
+    projectId: "hello-whiz-a3b9f",
+    storageBucket: "hello-whiz-a3b9f.appspot.com",
+    messagingSenderId: "1021678414701",
+    appId: "1:1021678414701:web:5b7fd97ea40c790b6bc864",
+    measurementId: "G-C9JWBN42G3"
+  };
